1d3b7649af7ca24d0a8842ecefc8569b16fe215f
Neil Montgomery <neilm@mie.utoronto.ca>
2015-11-16 10:08:05 -0500

    update

--- inference.R
+++ inference.R
@@ -1,28 +1,39 @@
 library(rio)
 library(dplyr)
 library(lubridate)
+library(ggplot2)
 
 
 service <- tbl_df(import("311_daily_contact_centre_trends.xlsx", sheet="all"))
 
+# Number of entries per year
+service %>% group_by(year(Date)) %>% summarise(n_days = n())
+
+# One sample t procedures
 service %>% filter(year(Date) == 2011) -> service_11
 mean(service_11$`Calls Answered %`)
 t.test(service_11$`Calls Answered %`, mu = 0.8)
 
+# Extract 2011 and 2012
 service %>% filter(year(Date) %in% c(2011, 2012)) -> service11_12
 
+# Boxplots
 service11_12 %>% ggplot(aes(x=factor(year(Date)), y=`Average Talk Time (sec)`)) +
   geom_boxplot()
 
+# Extract 2010:2013
 service %>% filter(year(Date) %in% 2010:2013) -> service_full
 
+# Boxplots
 service_full %>% ggplot(aes(x=factor(year(Date)), y=`Average Talk Time (sec)`)) +
   geom_boxplot()
 
+# Check out those outliers
 service_full %>% filter(year(Date)==2010) %>% 
   select(Date, `Calls Offered`, `Service Level %`, `Average Talk Time (sec)`) %>% 
   arrange(`Average Talk Time (sec)`)
 
+# Decision: remove them
 service %>% filter(year(Date) %in% 2010:2013, 
                    `Average Talk Time (sec)` > 100) -> service_full
 
